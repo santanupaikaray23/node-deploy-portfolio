8dfdea88a225d8a87fbce49a2c9903387b0b1260
kumar <santanupaikaray23@gmail.com>
2022-01-08 21:27:46 +0530

    add all files

---
+++ apiWithOutUi.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 9700;
+const mongo = require('mongodb');
+const MongoClient = mongo.MongoClient;
+const bodyParser = require('body-parser')
+const cors = require('cors');
+app.use(cors())
+let db;
+let col_name="customers"
+const mongourl ="mongodb+srv://naturewithcode:nature123@cluster0.yf62c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
+
+app.get('/health',(req,res)=>{
+    res.status(200).send('Health Check')
+});
+
+//Read
+app.get('/users',(req,res)=>{
+    db.collection(col_name).find().toArray((err,result)=>{
+        if(err) throw err;
+        res.send(result)
+    })
+})
+
+//Insert
+app.post('/addusers',(req,res)=>{
+    console.log(req.body)
+    db.collection(col_name).insert(req.body,(err,result)=>{
+        if(err) throw err;
+        res.send('Data Added')
+    })
+
+})
+//Db Connection
+MongoClient.connect(mongourl,(err,client)=>{
+    if(err) console.log('Error while connecting');
+    db= client.db('portfolio')
+    app.listen(port,(err)=>{
+        console.log(`Server is running on port ${port}`)
+    })
+})
